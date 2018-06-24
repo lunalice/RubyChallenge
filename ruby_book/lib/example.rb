3f0ec768d8f86d8fbb7f56f12a6ee2c82652bddd
辛い人 <35905452+lunalice@users.noreply.github.com>
2018-06-24 22:34:57 +0900

    【変更履歴】
    更新

--- ruby_book/lib/example.rb
+++ ruby_book/lib/example.rb
@@ -63,13 +63,75 @@ puts random_subdomain()
 def string_shuffle(s)
    s.split('').shuffle.join
 end
-string_shuffle("foobar")
+puts string_shuffle("foobar")
 
 # 演習4.3.3
 # 1
-a = { 'one'=>'uno','two'=>'dos','three'=>'tres'}
-a.map {|key,value| "#{key}のスペイン語は#{value}"}
+a = { 'one'=>'uno','two'=>'dos','three'=>'tres' }
+a.each {|key,value| p "#{key}のスペイン語は#{value}"}
 # 2
-person1 
-person2
-person3
+person1={:first=>'最初1',:last=>'最後1'}
+person2={:first=>'最初2',:last=>'最後2'}
+person3={:first=>'最初3',:last=>'最後3'}
+params={}
+params[:father]=person1
+params[:mother]=person2
+params[:child]=person3
+params.each do |key,value|
+   value.each {|key2,value2| p value2 }
+end 
+# 3
+user={}
+user[:name]='hoge'
+user[:email]='hogehuga@hogehuga'
+user[:password_digest]=("a".."z").to_a.shuffle[0..15].join
+# 4
+# 上書きされるupdateも類似
+buff = { "a" => 100, "b" => 200 }
+puts buff.merge({ "b" => 300 })
+
+# 演習4.4.1
+# 1 rangeコンストラクタ
+puts range1 = (1..10)
+# 2
+puts buff_range = Range.new(1,10)
+# 3
+if range1 == buff_range
+   puts '同じ'
+end
+
+# 演習4.4.2
+# 1
+puts Range.superclass
+puts Hash.superclass
+puts Symbol.superclass
+
+# 2
+class Word < String             # WordクラスはStringクラスを継承する
+  # 文字列が回文であればtrueを返す
+  def palindrome?
+    self == reverse        # selfは文字列自身を表します
+  end
+end
+s = Word.new('level')
+puts s.palindrome?
+
+# 演習4.4.3
+# 1
+class String             # WordクラスはStringクラスを継承する
+  # 文字列が回文であればtrueを返す
+  def palindrome?
+    self == self.reverse        # selfは文字列自身を表します
+  end
+  def shuffle
+    split('').shuffle.join
+  end
+end
+puts "racecar".palindrome?
+puts "onomatopoeia".palindrome?
+puts "Malayalam".downcase.palindrome?
+
+# 2 3
+puts "foobar".shuffle
+
+
